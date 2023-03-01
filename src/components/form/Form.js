0530645cfd8faf56afa211d8ea8ed8d7cf573cf2
Fene-87 <fenekayas87@gmail.com>
2023-03-01 14:04:03 +0300

    Implement functionality to add a book to the books array

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -1,15 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import uniqid from 'uniqid';
+import { addBook } from '../../redux/features/books/booksSlice';
 import './Form.css';
 
-const Form = () => (
-  <section className="formContainer">
-    <h2 className="header">Add New Book</h2>
-    <form className="bookForm">
-      <input className="bookTitle" type="text" placeholder="Book title" />
-      <input className="author" type="text" placeholder="Author" />
-      <button className="addBook" type="submit">Add Book</button>
-    </form>
-  </section>
-);
+const Form = () => {
+  const dispatch = useDispatch();
+  const [newTitle, setNewTitle] = useState('');
+  const [newAuthor, setNewAuthor] = useState('');
+  const newCategory = 'Action';
+
+  const handleAddBook = (e) => {
+    e.preventDefault();
+    dispatch(addBook(
+      {
+        itemId: uniqid(),
+        title: newTitle,
+        author: newAuthor,
+        category: newCategory,
+      },
+    ));
+  };
+
+  return (
+    <section className="formContainer">
+      <h2 className="header">Add New Book</h2>
+      <form className="bookForm">
+        <input
+          className="bookTitle"
+          type="text"
+          placeholder="Book title"
+          required
+          onChange={(e) => setNewTitle(e.target.value)}
+        />
+        <input
+          className="author"
+          type="text"
+          placeholder="Author"
+          required
+          onChange={(e) => setNewAuthor(e.target.value)}
+        />
+        <button
+          className="addBook"
+          type="submit"
+          onClick={handleAddBook}
+        >
+          Add Book
+        </button>
+      </form>
+    </section>
+  );
+};
 
 export default Form;
